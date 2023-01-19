0f7523ff330731efb0eedab9b4c9d81ab843c7f2
Elad Heller <eladhller@gmail.com>
2023-01-19 16:32:01 +0200

    handle paylod

--- index.mjs
+++ index.mjs
@@ -68,6 +68,65 @@ export const handler = async (event) => {
       };
 
     }
+    const payload = params.get('payload');
+    if (payload) {
+      const payloadObj = JSON.parse(payload);
+      if (payloadObj.type === 'view_submission') {
+        const text = payloadObj.view.state.values.input_c.plain_input.value;
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            response_action: 'update',
+            view: {
+              type: 'modal',
+              callback_id: 'view_1',
+              title: {
+                type: 'plain_text',
+                text: 'My App',
+                emoji: true,
+              },
+              submit: {
+                type: 'plain_text',
+                text: 'Submit',
+                emoji: true,
+              },
+              close: {
+                type: 'plain_text',
+                text: 'Cancel',
+                emoji: true,
+              },
+              blocks: [
+                {
+                  type: 'input',
+                  block_id: 'input_c',
+                  element: {
+                    type: 'plain_text_input',
+                    action_id: 'plain_input',
+                    placeholder: {
+                      type: 'plain_text',
+                      text: 'Enter some plain text',
+                    },
+                  },
+                  label: {
+                    type: 'plain_text',
+                    text: 'Plain text input',
+                    emoji: true,
+                  },
+                },
+                {
+                  type: 'section',
+                  text: {
+                    type: 'mrkdwn',
+                    text: `You entered: ${text}`,
+                  },
+                },
+              ],
+            },
+          }),
+        };
+      }
+    }
     return {
       statusCode: 200,
       body: JSON.stringify({
